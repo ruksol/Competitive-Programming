abc81a6c2f1cac83f7b3c1e1cff6d4d9fc2c0575
ruksol <86475757+ruksol@users.noreply.github.com>
2022-08-31 13:45:45 +0300

    grading students

--- grading students.cpp
+++ grading students.cpp
@@ -1,1 +1,13 @@
 //question link: https://www.hackerrank.com/challenges/grading/problem
+vector<int> gradingStudents(vector<int> grades) {
+    int n = grades.size();
+    for(int i=0; i<n; i++){
+        int x = (((grades[i]/5)*5)+5);
+        if(grades[i]>=38){
+            if(x-grades[i]<3){
+                grades[i]=x;
+            }
+        }
+    }
+    return grades;
+}
