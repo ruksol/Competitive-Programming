a4d0d71dc28c63e079542523bebbb52b6c755b13
ruksol <86475757+ruksol@users.noreply.github.com>
2022-10-02 20:24:39 +0300

    works for all cases

--- theatre_square.cpp
+++ theatre_square.cpp
@@ -3,27 +3,18 @@
 using namespace std;
 
 int main(){
-    int n, m, a;
+    unsigned long long int n, m, a;
 
     cin>>n>>m>>a;
 
-    int x = n*m;
-    int y = a*a;
-
-    if(x%y==0){
-        cout<<x/y;
-    }
-    else if(n==a && m!=a){
-        cout<<(x/y)+1;
-    }
-    else if(m==a && n!=a){
-        cout<<(x/y)+1;
-    }
-    else{
+    if(n%a!=0){
         n = (n/a + 1)*a;
-        m = (m/a + 1)*a;
-        x = n*m;
-        y = a*a;
-        cout<<x/y;
     }
+    if(m%a!=0){
+        m = (m/a + 1)*a;
+    }
+
+    cout<<(n*m)/(a*a);
+
+    return 0;
 }
