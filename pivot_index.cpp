5d2ab1b685b28e7b4bcf642a1c9c185f8042cd30
ruksol <86475757+ruksol@users.noreply.github.com>
2022-10-17 18:40:27 +0300

    https://www.youtube.com/watch?v=HhCzL4ciCD4

---
+++ pivot_index.cpp
@@ -0,0 +1,20 @@
+//question link: https://leetcode.com/problems/find-pivot-index/
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int leftSum = 0;
+        int rightSum = 0;
+        for(int i=0; i<nums.size(); i++){
+            rightSum += nums[i];
+        }
+        for(int i=0; i<nums.size(); i++){
+            rightSum -= nums[i];
+            if(leftSum == rightSum){
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+};
